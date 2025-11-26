cc9fa96afb84ec4dc0210598ae50996f34bb0411
squidnose <ozfiala@gmail.com>
2025-11-26 19:16:17 +0100

    now able to setup new server

---
+++ scripts/2.Manage-Servers.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#==================================== MC Server Managment ====================================
+TITLE="MC server Managment"
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+MC_ROOT="$HOME/mcservers"
