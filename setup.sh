3acfa24ea143510257e32d69184655b7ced1a7ed
squidnose <ozfiala@gmail.com>
2025-12-24 16:01:59 +0100

    Added jre to deps

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ echo "2. Debian 12(Not reccomended)"
 echo "3. Debian 13(Not reccomended)"
 echo "4. Voidlinux"
 echo "5. Archlinux(Untested)"
-echo "6. Fedora(Untested)"
+echo "6. Fedora(Untested, Java 8 and 17 removed???)"
 echo "7. Not on the list?"
 read -r -p "Enter your choice (1-6):" CHOICE
 case $CHOICE in
@@ -16,13 +16,13 @@ case $CHOICE in
 echo "Installing MCserverTUI dependecies"
 sudo apt install newt python3 tmux curl wget
 echo "Installing Minecraft Java dependecies"
-sudo apt install openjdk-8-jdk-headless openjdk-17-jdk-headless openjdk-21-jdk-headless
+sudo apt install openjdk-8-jdk-headless openjdk-8-jre-headless openjdk-17-jdk-headless openjdk-17-jre-headless openjdk-21-jdk-headless openjdk-21-jre-headless
 ;;
 2)
 echo "Installing MCserverTUI dependecies"
 sudo apt install newt python3 tmux curl wget
 echo "Installing Minecraft Java dependecies"
-sudo apt install openjdk-17-jdk-headless
+sudo apt install openjdk-17-jdk-headless openjdk-17-jre-headless
 echo "DEBIAN 12 DOESNT SUPPORT JAVA 21 FOR MC 1.20.5 AND NEWER"
 echo "DEBIAN 12 DOESNT SUPPORT JAVA 8 FOR MC 1.16.5 AND OLDER"
 echo "Either manually install Java 21 and 8 or use a diferent distro..."
@@ -31,7 +31,7 @@ echo "Either manually install Java 21 and 8 or use a diferent distro..."
 echo "Installing MCserverTUI dependecies"
 sudo apt install newt python3 tmux curl wget
 echo "Installing Minecraft Java dependecies"
-sudo apt install openjdk-21-jdk-headless
+sudo apt install openjdk-21-jdk-headless openjdk-21-jre-headless
 echo "DEBIAN 13 DOESNT SUPPORT JAVA 17 or 8 FOR MC 1.20.4 AND OLDER"
 echo "Either manually install Java 17 and 8 or use a diferent distro..."
 ;;
@@ -41,7 +41,9 @@ sudo xbps-install -Syu newt cronie python3 tmux curl wget
 echo "Enabling Crontab service"
 sudo ln -s /etc/sv/cronie/ /var/service/
 echo "Installing Minecraft Java dependecies"
-sudo xbps-install -Syu openjdk8 openjdk17 openjdk21
+sudo xbps-install -Syu openjdk8 openjdk8-jre openjdk17 openjdk17-jre openjdk21 openjdk21-jre
+echo "Installing nerd font symbols"
+sudo xbps-install -Syu nerd-fonts-symbols-ttf
 ;;
 5)
 echo "Installing MCserverTUI dependecies"
@@ -49,7 +51,7 @@ sudo pacman -S newt cronie python tmux curl wget
 echo "Enabling Crontab service"
 sudo systemctl enable cronie.service
 echo "Installing Minecraft Java dependecies"
-sudo pacman -S  jdk8-openjdk jdk17-openjdk jdk21-openjdk
+sudo pacman -S  jdk8-openjdk jre8-openjdk-headless jdk17-openjdk jre17-openjdk-headless jdk21-openjdk jre21-openjdk-headless
 ;;
 6)
 echo "Installing MCserverTUI dependecies"
@@ -58,6 +60,7 @@ echo "Enabling Crontab service"
 sudo systemctl enable cronie.service
 echo "Installing Minecraft Java dependecies"
 sudo dnf install java-1.8.0-openjdk java-17-openjdk java-21-openjdk
+echo "If you got an error, java 8 and 17 are probably no longer in fedora..."
 ;;
 7)
 echo "If you distro is not listed or it did not work."
